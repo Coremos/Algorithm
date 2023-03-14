13aa9fdd4c7f3a609cc8ec95e4073fe4ce2e01d5
Coremos <47705451+Coremos@users.noreply.github.com>
2023-03-14 15:34:43 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    int score;
+    
+    std::cin >> score;
+    
+    if (score > 89)
+    {
+        std::cout << "A";
+    }
+    else if (score > 79)
+    {
+        std::cout << "B";
+    }
+    else if (score > 69)
+    {
+        std::cout << "C";
+    }
+    else if (score > 59)
+    {
+        std::cout << "D";
+    }
+    else
+    {
+        std::cout << "F";
+    }
+}
